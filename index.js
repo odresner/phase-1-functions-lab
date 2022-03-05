d6f3d2ad657b2cb7ce2a8f1050e5e9f6dece2a56
Owen Dresner <owendrez@gmail.com>
2022-03-05 14:43:46 -0500

    commit tree 08e87ca77c4d45cf94e9e7389b388e4e8585e51f

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(block){
+    if(block<42){
+        return 42-block;
+    }else if(block>42){
+        return block-42;
+    }else{
+        return 0;
+    }
+}
+function distanceFromHqInFeet(block){
+    if(block<42){
+        return (42-block)*264
+    }else if(block>42){
+        return (block-42)*264
+    }else{
+        return 0
+    }
+}
+function distanceTravelledInFeet(start, end){
+    if(start>end){
+        return (start-end)*264
+    }else if(start<end){
+        return (end-start)*264
+    }
+}
+function calculatesFarePrice(start, end){
+    let distance=0;
+    if(start>end){
+        distance=(start-end)*264
+    }else if(start<end){
+        distance=(end-start)*264
+    }
+    if(distance<=400){
+        return 0
+    }else if(distance>400&&distance<=2000){
+        return ((distance*2)-800)/100
+    }else if(distance>2000&&distance<=2500){
+        return 25
+    }else{
+        return "cannot travel that far"
+    }
+
+}
+
